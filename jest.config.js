8505f097365b266d5c583652fa149ffe0cf4cc02
Leone Perdigão <leone.lage@gmail.com>
2020-05-10 13:26:31 +0200

    Generate badges from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,16 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.ts",
+    'src/**/*.ts',
   ],
-  coverageDirectory: "./.coverage",
-  moduleFileExtensions: ["ts", "js"],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageDirectory: './.coverage',
+  moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
-  testEnvironment: "node",
-  testRegex: "(__tests__\/.*\.test\.ts)$",
+  testEnvironment: 'node',
+  testRegex: '(__tests__/.*.test.ts)$',
   transform: {
-    ".ts": "ts-jest"
+    '.ts': 'ts-jest',
   },
-  verbose: true
+  verbose: true,
 };
--- lib/diffMapper.d.ts
+++ lib/diffMapper.d.ts
@@ -18,6 +18,20 @@ export default class DiffMapper {
      * @throws an error if a non-object is given
      */
     static map(source: any, target: any): DiffMapResult;
+    /**
+     *
+     * @param diff
+     * @returns DiffMapResult represented as an array
+     * @throws an error if no diff result is passed as parameter
+     */
+    static toArray(diff: DiffMapResult): [string, DiffMapResult][];
+    /**
+     *
+     * @param diff
+     * @returns DiffMapResult stringified
+     * @throws an error if no diff result is passed as parameter
+     */
+    static toString(diff: DiffMapResult): string;
     private static compareValues;
     private static isFunction;
     private static isArray;
